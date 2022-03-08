577980615a26a17c3eb3c183066197d786f9921c
Kevin Filipak <kevinaguiar09@gmail.com>
2022-03-08 08:52:48 -0300

    Model

--- UTP.PI.2022.1.Forms/Program.cs
+++ UTP.PI.2022.1.Forms/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace UTP.PI._2022._1.Forms
 {
@@ -14,6 +17,34 @@ namespace UTP.PI._2022._1.Forms
         [STAThread]
         static void Main()
         {
+
+            if (!File.Exists("DB.sqlite"))
+            {
+
+                SqliteConnection conn = new SqliteConnection("Data Source=DB.sqlite");
+
+                conn.Open();
+
+                SqliteCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "CREATE TABLE Banco ([Id] INTEGER PRIMARY KEY, [Nome] VARCHAR(100));";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "CREATE TABLE Agencia ([Id] INTEGER PRIMARY KEY, [Nome] VARCHAR(100), [Endereco] VARCHAR(100), [CodigoBanco] INTEGER);";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "CREATE TABLE Cliente ([Id] INTEGER PRIMARY KEY, [Nome] VARCHAR(100), [CPF] VARCHAR(100), [Email] VARCHAR(100), [Telefone] VARCHAR(100));";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "CREATE TABLE Conta ([Id] INTEGER PRIMARY KEY, [NumeroConta] VARCHAR(100), [CodigoAgencia] INTEGER, [CodigoCliente] INTEGER, [Limite] FLOAT, [Saldo] FLOAT);";
+                cmd.ExecuteNonQuery();
+
+                cmd.Dispose();
+
+                conn.Close();
+                conn.Dispose();
+            }
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
